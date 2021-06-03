e2c8749977e5ec36305e294fe9294e5b6229bf08
DenisKostyukov <kostukov08@gmail.com>
2021-06-03 09:01:30 +0300

    refactor isCurrentMonth and isWeekend in component Day

--- src/Components/Calendar/CalendarBody/Week/Day/index.jsx
+++ src/Components/Calendar/CalendarBody/Week/Day/index.jsx
@@ -4,17 +4,11 @@ import cn from 'classnames';
 import style from './Day.module.sass';
 function Day (props) {
   const { currentDay } = props;
-  const isCurrentMonth = () => {
-    if (format(currentDay, 'M') === format(new Date(), 'M')) {
-      return true;
-    }
-  };
-  const isWeekEnd = () => {
-    if (isWeekend(currentDay)) {
-      return true;
-    }
-  };
-  
+  const isCurrentMonth = () =>
+    format(currentDay, 'M') === format(new Date(), 'M');
+
+  const isWeekEnd = () => isWeekend(currentDay);
+
   return (
     <>
       <div
@@ -22,7 +16,9 @@ function Day (props) {
           style.day,
           isCurrentMonth() ? '' : style.otherMonth,
           isWeekEnd() && isCurrentMonth() ? style.weekend : '',
-          currentDay.toLocaleDateString() === new Date().toLocaleDateString() ? style.currentDay : ''
+          currentDay.toLocaleDateString() === new Date().toLocaleDateString()
+            ? style.currentDay
+            : ''
         )}
       >
         {format(currentDay, 'dd')}
